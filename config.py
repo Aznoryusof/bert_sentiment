48ff885e3f90523dfdee9b0aa7cff4decb2c9344
Aznor Yusof <Aznor1990@gmail.com>
2020-06-25 13:22:55 +0800

    insert prediction text variable

--- config.py
+++ config.py
@@ -17,3 +17,5 @@ MAX_LEN = 128
 batch_size = 32
 epochs = 2
 use_gpu_predict = False
+
+prediction_text = "Ive probably stayed there 5 weeks in the last few months. Had to extend my latest stay by another 8 days and asked if I could get a late checkout due to a late flight. Basically refused and didn't offer anything in recognition of how much I've stayed there recently. the manager there is an idiot. Have moved to the Y Hotel. Not much more and a thousand times better Hotel. i won't be back X Hotel. You blew it!"
